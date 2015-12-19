777c5702368eb0fa60523466621b62ce005528b5
Yuminaka <umxum@me.com>
2015-12-19 16:08:21 +0900

    最終

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(function() {
+
+  jQuery(document).ready(function($){
+						$('.bxslider').bxSlider({
+								auto: true,
+								pause: 3500,
+								speed: 800,
+								mode: 'horizontal',
+								infiniteLoop: true,
+						});
+			});
+});
